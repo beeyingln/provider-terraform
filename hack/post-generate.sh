6ea04f4938350e58fa28ca173e84f8a69c2df01f
vagrant <bee_ying.lo@nokia.com>
2022-08-03 10:44:45 +0000

    Add Post Generation Templates : Rename templates

--- hack/post-generate.sh
+++ hack/post-generate.sh
@@ -11,32 +11,32 @@ rm -v !("${ProviderNameLower}.jet.crossplane.io_providerconfigs.yaml"|"${Provide
 cd ../..
 
 
-cp xno-templates/package/crds/resource.terraform.jet.crossplane.io_resources.yaml.tmpl package/crds/resource.${ProviderNameLower}.jet.crossplane.io_resources.yaml
+cp xno-tmpls/package/crds/resource.terraform.jet.crossplane.io_resources.yaml.tmpl package/crds/resource.${ProviderNameLower}.jet.crossplane.io_resources.yaml
 sed -i "s/<PROVIDER_NAME>/${ProviderNameLower}/g" "package/crds/resource.${ProviderNameLower}.jet.crossplane.io_resources.yaml"
 
 mkdir internal/controller/resource
-cp -R xno-templates/internal/controller/resource/zz_controller.go.tmpl internal/controller/zz_controller.go
+cp xno-tmpls/internal/controller/resource/zz_controller.go.tmpl internal/controller/resource/zz_controller.go
 sed -i "s/<PROVIDER_NAME>/${ProviderNameLower}/g" internal/controller/resource/zz_controller.go
 
-cp xno-templates/internal/controller/zz_setup.go.tmpl internal/controller/zz_setup.go
+cp xno-tmpls/internal/controller/zz_setup.go.tmpl internal/controller/zz_setup.go
 sed -i "s/<PROVIDER_NAME>/${ProviderNameLower}/g" internal/controller/zz_setup.go
 
 mkdir examples/resource
-cp xno-templates/examples/resource/resource.yaml.tmpl examples/resource/resource.yaml
+cp xno-tmpls/examples/resource/resource.yaml.tmpl examples/resource/resource.yaml
 sed -i "s/<PROVIDER_NAME>/${ProviderNameLower}/g" examples/resource/resource.yaml
 
 sed -i "s/<PROVIDER_NAME>/${ProviderNameLower}/g" config/resource/config.go
 
 #Update Schema json
 
-mkdir -f apis/resource/v1alpha1
-cp -R xno-templates/apis/resource/v1alpha1/zz_generated.deepcopy.go.tmpl apis/resource/v1alpha1/zz_generated.deepcopy.go 
-cp -R xno-templates/apis/resource/v1alpha1/zz_generated.managed.go.tmpl apis/resource/v1alpha1/zz_generated.managed.go
-cp -R xno-templates/apis/resource/v1alpha1/zz_generated.managedlist.go.tmpl apis/resource/v1alpha1/zz_generated.managedlist.go
-cp -R xno-templates/apis/resource/v1alpha1/zz_generated_terraformed.go.tmpl apis/resource/v1alpha1/zz_generated_terraformed.go
-cp -R xno-templates/apis/resource/v1alpha1/zz_groupversion_info.go.tmpl apis/resource/v1alpha1/zz_groupversion_info.go
-cp -R xno-templates/apis/resource/v1alpha1/zz_resource_types.go.tmpl apis/resource/v1alpha1/zz_resource_types.go
+mkdir -p apis/resource/v1alpha1
+cp xno-tmpls/apis/resource/v1alpha1/zz_generated.deepcopy.go.tmpl apis/resource/v1alpha1/zz_generated.deepcopy.go 
+cp xno-tmpls/apis/resource/v1alpha1/zz_generated.managed.go.tmpl apis/resource/v1alpha1/zz_generated.managed.go
+cp xno-tmpls/apis/resource/v1alpha1/zz_generated.managedlist.go.tmpl apis/resource/v1alpha1/zz_generated.managedlist.go
+cp xno-tmpls/apis/resource/v1alpha1/zz_generated_terraformed.go.tmpl apis/resource/v1alpha1/zz_generated_terraformed.go
+cp xno-tmpls/apis/resource/v1alpha1/zz_groupversion_info.go.tmpl apis/resource/v1alpha1/zz_groupversion_info.go
+cp xno-tmpls/apis/resource/v1alpha1/zz_resource_types.go.tmpl apis/resource/v1alpha1/zz_resource_types.go
 #TODO Replace
 
-cp xno-templates/apis/zz_register.go.tmpl apis/zz_register.go
+cp xno-tmpls/apis/zz_register.go.tmpl apis/zz_register.go
 sed -i "s/<PROVIDER_NAME>/${ProviderNameLower}/g" apis/zz_register.go

38986e7a0a496efbad4ad9132a9d9f77c33521d4
vagrant <bee_ying.lo@nokia.com>
2022-08-06 16:56:40 +0000

    Add Comment + Bug fixing on accidental replacement of template keyword

--- generate-netbox-provider.sh
+++ generate-netbox-provider.sh
@@ -1,10 +1,9 @@
-sed -i "s/\/\/REPLACE-TO-LOCAL\/\//replace github.com\/crossplane\/terrajet => \/home\/vagrant\/workspace\/spike\/terrajet/g" go.mod
+export ProviderNameLower=netbox
+export ProviderNameUpper=Netbox
+export TERRAFORM_PROVIDER_SOURCE="e-breuninger/netbox"
+export TERRAFORM_PROVIDER_VERSION="2.0.2"
+export TERRAFORM_PROVIDER_DOWNLOAD_NAME="terraform-provider-netbox"
+export TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX="https://github.com/e-breuninger/terraform-provider-netbox/releases/download/v2.0.2"
+export TERRAFORM_NATIVE_PROVIDER_BINARY="terraform-provider-netbox_v2.0.2"
 
-ProviderNameLower=netbox \
-ProviderNameUpper=Netbox \
-TERRAFORM_PROVIDER_SOURCE="e-breuninger/netbox" \
-TERRAFORM_PROVIDER_VERSION="2.0.2" \
-TERRAFORM_PROVIDER_DOWNLOAD_NAME="terraform-provider-netbox" \
-TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX="https://github.com/e-breuninger/terraform-provider-netbox/releases/download/v2.0.2" \
-TERRAFORM_NATIVE_PROVIDER_BINARY="terraform-provider-netbox_v2.0.2" \
-make generate-provider
+./generate-provider.sh
---
+++ generate-provider.sh
@@ -0,0 +1,24 @@
+export ProviderNameLower="${ProviderNameLower:-'github'}"
+export ProviderNameUpper"${ProviderNameUpper:-'Github'}"
+
+export TERRAFORM_PROVIDER_SOURCE="${TERRAFORM_PROVIDER_SOURCE:-'integrations/github'}"
+export TERRAFORM_PROVIDER_VERSION="${TERRAFORM_PROVIDER_VERSION:-'4.19.2'}"
+export TERRAFORM_PROVIDER_DOWNLOAD_NAME="${TERRAFORM_PROVIDER_DOWNLOAD_NAME:-'terraform-provider-github'}"
+export TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX="${TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX:-'https://releases.hashicorp.com/terraform-provider-github/4.19.2'}"
+export TERRAFORM_NATIVE_PROVIDER_BINARY="${TERRAFORM_NATIVE_PROVIDER_BINARY:-'terraform-provider-github_4.19.2'}"
+
+echo "Start Generate Terraform Provider"
+echo "ProviderNameLower=$ProviderNameLower"
+echo "ProviderNameUpper=$ProviderNameUpper"
+
+echo "TERRAFORM_PROVIDER_SOURCE=${TERRAFORM_PROVIDER_SOURCE}"
+echo "TERRAFORM_PROVIDER_VERSION=${TERRAFORM_PROVIDER_VERSION}"
+echo "TERRAFORM_PROVIDER_DOWNLOAD_NAME=${TERRAFORM_PROVIDER_DOWNLOAD_NAME}"
+echo "TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX=${TERRAFORM_PROVIDER_DOWNLOAD_URL_PREFIX}"
+echo "TERRAFORM_NATIVE_PROVIDER_BINARY=${TERRAFORM_NATIVE_PROVIDER_BINARY}"
+
+
+sed -i "s/\/\/REPLACE-TO-LOCAL\/\//replace github.com\/crossplane\/terrajet => \/home\/vagrant\/workspace\/spike\/terrajet/g" go.mod
+
+
+make generate-provider
